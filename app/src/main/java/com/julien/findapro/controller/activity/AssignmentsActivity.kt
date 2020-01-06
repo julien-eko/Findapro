5080ada1165e7c88ee74900d4b7046ebba94e7a5
julien-eko <julien.darcos@gmail.com>
2020-01-06 11:49:43 +0100

    Assignments activity design

--- app/src/main/java/com/julien/findapro/controller/activity/AssignmentsActivity.kt
+++ app/src/main/java/com/julien/findapro/controller/activity/AssignmentsActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.julien.findapro.R
@@ -17,18 +18,49 @@ class AssignmentsActivity : AppCompatActivity() {
 
         configureToolbar()
 
+        loadDb()
+
         assignments_save_button.setOnClickListener {
-           saveInDb()
+            if(activity_assignments_describe_edit_text.text.toString().trim() == ""){
+                Toast.makeText(this,getString(R.string.no_blank_field),Toast.LENGTH_SHORT).show()
+            }else{
+                saveInDb()
+            }
+
+        }
+        assignments_finish_button.setOnClickListener {
+            finish()
         }
     }
 
+    private fun loadDb(){
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("pro users").document(intent.getStringExtra("proId")).get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    activity_assignements_full_name_text_view.text = document["full name"].toString()
+                    activity_assignements_job_text_view.text = document["job"].toString()
+                    activity_assignment_ratingbar.rating = document["rating"].toString().toFloat()
+
+                    val nbRating:String = "(" + document["ratingNb"].toString() + ") : "
+                    activity_assignements_nb_rating_text_view.text = nbRating
+                } else {
+                    Log.d("load document pro user", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("load document pro user", "get failed with ", exception)
+            }
+    }
     private fun saveInDb(){
         val db = FirebaseFirestore.getInstance()
+
 
         val assignments = hashMapOf(
             "user id" to FirebaseAuth.getInstance().currentUser?.uid!!,
             "pro user id" to intent.getStringExtra("proId"),
-            "describe" to "test",
+            "describe" to activity_assignments_describe_edit_text.text.toString(),
             "status" to "pending"
         )
 
@@ -49,7 +81,7 @@ class AssignmentsActivity : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_white_24)
         actionBar?.setDisplayHomeAsUpEnabled(true)
-        actionBar?.setTitle("!!!")
+        actionBar?.setTitle("Demande d'intervention")
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
